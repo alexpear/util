d69319ccbc313403afcc95b590e785b83f72ef30
Alex Pear <alexmpear@gmail.com>
2016-05-24 15:44:11 -0400

    Standardize indentation to 4 spaces per unit

--- backup.py
+++ backup.py
@@ -6,42 +6,42 @@ import shutil
 import sys
 
 def backup(filepaths):
-	# arg is relative path to what we want to backup
-	if len(filepaths) < 1:
-	  print('usage: backup filepath...')
-	  return
+    # arg is relative path to what we want to backup
+    if len(filepaths) < 1:
+        print('usage: backup filepath...')
+        return
 
-	# check the relative path(s) are legit
-	for filepath in filepaths:
-		if not os.path.exists(filepath):
-			print('error: couldnt find "{file}", relative to current directory'.format(file=filepath))
-			return
+    # check the relative path(s) are legit
+    for filepath in filepaths:
+        if not os.path.exists(filepath):
+            print('error: couldnt find "{file}", relative to current directory'.format(file=filepath))
+            return
 
-	dt = datetime.datetime.now()
+    dt = datetime.datetime.now()
 
-	# TODO: year support
-	datestr = "{month:02}{day:02}".format(month=dt.month, day=dt.day)
-	timestr = "{hour:02}{minute:02}".format(hour=dt.hour, minute=dt.minute)
+    # TODO: year support
+    datestr = "{month:02}{day:02}".format(month=dt.month, day=dt.day)
+    timestr = "{hour:02}{minute:02}".format(hour=dt.hour, minute=dt.minute)
 
-	# do dirs corresopnding to today's date exist?
-	backups_path = os.path.expanduser('~/backups/')
+    # do dirs corresopnding to today's date exist?
+    backups_path = os.path.expanduser('~/backups/')
 
-	if not os.path.isdir(backups_path + datestr):
-	  os.mkdir(backups_path + datestr)
+    if not os.path.isdir(backups_path + datestr):
+        os.mkdir(backups_path + datestr)
 
-	desired_path = backups_path + datestr + '/' + timestr + '/'
-	if not os.path.isdir(desired_path):
-	  os.mkdir(desired_path)
+    desired_path = backups_path + datestr + '/' + timestr + '/'
+    if not os.path.isdir(desired_path):
+        os.mkdir(desired_path)
 
-	for filepath in filepaths:
-		if os.path.exists(desired_path + os.path.basename(filepath)):
-			# if already exists in backups dir...
-			print('ERROR: file "{file}" already backed up here, not overwriting'.format(file=filepath))
-			continue
-		else:
-			shutil.copy(filepath, desired_path)
-			print('copying {file}'.format(file=os.path.basename(filepath)))
+    for filepath in filepaths:
+        if os.path.exists(desired_path + os.path.basename(filepath)):
+            # if already exists in backups dir...
+            print('ERROR: file "{file}" already backed up here, not overwriting'.format(file=filepath))
+            continue
+        else:
+            shutil.copy(filepath, desired_path)
+            print('copying {file}'.format(file=os.path.basename(filepath)))
 
-	print('backup complete thank you :)')
+    print('backup complete thank you :)')
 
 backup(sys.argv[1:])
