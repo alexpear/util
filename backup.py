6bf337e2f5429c5a2245e78ad3a18b378a21b0ea
alexpear <alex@havenlife.com>
2020-03-05 16:06:54 -0500

    Year support, improve logging

--- backup.py
+++ backup.py
@@ -20,26 +20,30 @@ def backup(filepaths):
 
     dt = datetime.datetime.now()
 
-    # TODO: year support
+    yearstr = "{year:04}".format(year=dt.year)
     datestr = "{month:02}{day:02}".format(month=dt.month, day=dt.day)
     timestr = "{hour:02}{minute:02}".format(hour=dt.hour, minute=dt.minute)
 
     # do dirs corresopnding to today's date exist?
     backups_path = os.path.expanduser('~/backups/')
 
-    if not os.path.isdir(backups_path + datestr):
-        os.mkdir(backups_path + datestr)
+    if not os.path.isdir(backups_path + yearstr):
+        os.mkdir(backups_path + yearstr)
 
-    desired_path = backups_path + datestr + '/' + timestr + '/'
+    date_path = backups_path + yearstr + '/' + datestr + '/'
+    if not os.path.isdir(date_path):
+        os.mkdir(date_path)
+
+    desired_path = date_path + timestr + '/'
     if not os.path.isdir(desired_path):
         os.mkdir(desired_path)
 
     for filepath in filepaths:
         if os.path.exists(desired_path + os.path.basename(filepath)):
-            # TODO: Do not print smiley face at end if anything fails to backup
+            # TODO: Instead, do back it up with a incremented filename
             # if already exists in backups dir...
             print('ERROR: file "{file}" already backed up here, not overwriting'.format(file=filepath))
-            continue
+            return
         else:
             shutil.copy(filepath, desired_path)
             print('copying {file}'.format(file=os.path.basename(filepath)))
